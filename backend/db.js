9f5698e4c3332b540ef444b19b3bec274f0b8d7c
jahnvigangwar <jahnvigangwar@gmail.com>
2023-06-01 14:19:04 +0530

    b4 display bk on frnt end

--- backend/db.js
+++ backend/db.js
@@ -7,9 +7,20 @@ const mongoDB = async()=>{
         else{
             console.log("Mongo Connectd");
             const fetched_data = await mongoose.connection.db.collection("food_items");
-            fetched_data.find({}).toArray(function(err,data){
-                if (err) console. log ("----",err)
-                else console.log("connected at the backend");
+            fetched_data.find({}).toArray( async function(err,data){
+
+                const foodcategory = await mongoose.connection.db.collection("foodcategory");
+                foodcategory.find({}).toArray(function(err,catData)
+                {
+                if (err) console.log (err);
+                else{
+                    console.log("connected at the backend");
+                    global.food_items = data;
+                    global.foodcategory = catData; 
+                    // console.log(global.food_items);
+                    // console.log(global.foodcategory)
+                } 
+                })
             })
         }
 
